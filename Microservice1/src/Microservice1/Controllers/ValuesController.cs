f7651ed16c5bcd0ab2ed7b836d42e9234830d157
Waldyr Felix <waldyrfelix@gmail.com>
2015-05-15 00:03:37 -0300

    microservice 1

---
+++ Microservice1/src/Microservice1/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Microservice1.Controllers
+{
+    [Route("Estoque")]
+    public class EstoqueController : Controller
+    {
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new { microservice = "Produtos em estoque" };
+        }
+
+    }
+}
---
+++ Microservice1/src/Microservice1/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Microservice1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
