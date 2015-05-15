c2f6796850f5f458efdb2111abf733e8d46a3e47
Waldyr Felix <waldyrfelix@gmail.com>
2015-05-15 02:06:56 -0300

    Dockerfile novo

--- Microservice1/src/Microservice1/Controllers/EstoqueController.cs
+++ Microservice1/src/Microservice1/Controllers/EstoqueController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Mvc;
 
 namespace Microservice1.Controllers
 {
-    [Route("Estoque")]
+    [Route("api/Estoque")]
     public class EstoqueController : Controller
     {
         [HttpGet]
